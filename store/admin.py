545f674d5402281ef34a47f31c7870d5d3a8c6ee
Lekhman-vold <vladgrojsman6@gmail.com>
2021-04-08 17:41:24 +0300

    first commit

---
+++ store/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(ModelAdmin):
+    pass
---
+++ store/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrStaffReadOnly
+
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsOwnerOrStaffReadOnly]
+    filter_fields = ['price']
+    search_fields = ['name', 'author_name']
+    ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
+
+
+def auth(request):
+    return render(request, 'oauth.html')
